87eb825506e3605d66edb60ea295ff9bf6031e8a
Sakuraiii <denblaze@mail.ru>
2021-07-21 00:53:56 +0300

    First commit. Simple website for HH.ru

--- src/main/java/com/example/blog/controllers/MainController.java
+++ src/main/java/com/example/blog/controllers/MainController.java
@@ -1,11 +1,10 @@
 package com.example.blog.controllers;
 
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+
 @Controller
 public class MainController {
     @GetMapping("/")
