8c369c7e52253bc89c3a56a22482bd6bf3066b1f
Sakuraiii <denblaze@mail.ru>
2021-07-20 19:36:49 +0300

    make it better

--- src/main/java/com/example/blog/controllers/MainController.java
+++ src/main/java/com/example/blog/controllers/MainController.java
@@ -1,12 +1,13 @@
 package com.example.blog.controllers;
 
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @Controller
 public class MainController {
-
     @GetMapping("/")
     public String home(Model model) {
         model.addAttribute("title", "MainPage");
